0cb6a7f0ebaeaa055a8feec9c5a069a95020f0d1
Alvaro <astaboada7@gmail.com>
2021-01-10 01:04:07 +0100

    ayuda de giacomo, delete medio hecho

--- main.js
+++ main.js
@@ -1,16 +1,76 @@
-let ToDo = ["trilero", "async/promises", "myTryLaravel", "triogger css animations from js", "hack money-maker-machine"]
 
-function showList (ToDo) {
-    const container = document.getElementById("list");
-    const list = document.createElement('ul');
+let ToDo = [
+  "trilero",
+  "async/promises",
+  "myTryLaravel",
+  "trigger css animations from js",
+  "sequence CSS animations in chain",
+  "hack money-maker-machine",
+  "finally rule the world",
+];
 
-    for(let i = 0; i < ToDo.length; i++) {
-        let listElement = document.createElement('li');
-        listElement.appendChild(document.createTextNode(ToDo[i]));
-        list.appendChild(listElement);
-        container.appendChild(list);
+//const container = document.getElementById("list");
+const list = document.getElementById("list");
+
+
+function showList(ToDo) {
+
+    for (let i = 0; i < ToDo.length; i++) {
+        list.innerHTML += `
+            <li class="buttons">
+            <span> ${ToDo[i]} </span> 
+            <div>
+                <button type="button" onclick="deleteItem(${i})">dilit</button>
+                <button type="button" onclick="editItem(${i})">idit</button>
+            </div>
+            </li>
+        `;
     }
-    return list;
+    
+ 
+//   for (let i = 0; i < ToDo.length; i++) {
+//     let listElement = document.createElement("li");
+//     listElement.appendChild(document.createTextNode(ToDo[i]));
+//     let button = document.createElement("BUTTON");
+//     button.innerHTML = "dilit";
+//     listElement.appendChild(button);
+//     list.appendChild(listElement);
+//     container.appendChild(list);
+//   }
+  //return list;
+
+  //locationReload(), set Timeout
+
+  
 }
+
 console.log(ToDo);
 showList(ToDo);
+
+
+
+function resolveAfterHalfSec () {
+    return new Promise(resolve => {
+        setTimeout (() => {
+            resolve('resolved');
+        }, 500);
+    });
+}
+
+async function deleteItem(indiceArray) {
+    console.log(ToDo);
+
+    await resolveAfterHalfSec(ToDo.splice(indiceArray, 1));
+    console.log(ToDo);
+    
+    location.reload();
+}
+
+
+console.log(ToDo);
+
+function addListItem() {
+  //pillar el input de form
+  //asociarlo a una variable string
+  //pushearlo al array
+}
